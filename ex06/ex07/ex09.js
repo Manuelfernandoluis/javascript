897de4819a496a85e4c2620b664d78ae85c26164
Manuel Luís <manuelwisgy16@gmail.com>
2024-05-14 01:14:01 +0100

    condições

---
+++ ex06/ex07/ex09.js
@@ -0,0 +1,8 @@
+//Para escrever na tela no node.js nós temos que usar console.log e não document.write
+//Identação em programação são tecnicas de organização para organizar os nossos códigos
+ var vel=80 //Essa condição agente classifica como uma condição simples, porque ele não tem se não
+ console.log(`A velocidade do seu carro é ${vel} kilometro/hora`)
+if(vel>60){
+    console.log('Você passou a velocidade permitida, terá que ser multado!')
+}
+ console.log('Dirija sempre com o sinto de segurança')
